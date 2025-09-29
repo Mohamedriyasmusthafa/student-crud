512325ad4fed5bd91cf14a2da62762022e3353f8
Mohamedriyasmusthafa <riyasmusthafa9047@gmail.com>
2025-09-29 14:43:12 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,19 +1,23 @@
-CREATE DATABASE school;
+-- Create database
+CREATE DATABASE IF NOT EXISTS school;
 
-USE students_db;
-CREATE TABLE students (
-    id INT PRIMARY KEY,
+-- Select the database
+USE school;
+
+-- Create students table
+CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     roll_no INT NOT NULL,
     student_name VARCHAR(100) NOT NULL,
     section VARCHAR(5),
     overall_marks INT
 );
 
-
-INSERT INTO students (id, roll_no, student_name, section, overall_marks) VALUES
-(1, 1, 'Mohamed Riyas', 'A', 88),
-(2, 2, 'Ayesha Khan', 'B', 92),
-(3, 3, 'Arjun Kumar', 'A', 78),
-(4, 4, 'chandruu', 'A', 99),
-(5, 5, 'adhi', 'B', 80),
-(6, 6, 'joe', 'A', 0);
+-- Insert sample records
+INSERT INTO students (roll_no, student_name, section, overall_marks) VALUES
+(1, 'Mohamed Riyas', 'A', 88),
+(2, 'Ayesha Khan', 'B', 92),
+(3, 'Arjun Kumar', 'A', 78),
+(4, 'Chandruu', 'A', 99),
+(5, 'Adhi', 'B', 80),
+(6, 'Joe', 'A', 0);
